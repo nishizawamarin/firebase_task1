dcbfffb8a6622eebb121a103d027ce783c867eea
RyoNakahara072144 <84167380+RyoNakahara072144@users.noreply.github.com>
2023-05-05 00:11:00 +0900

    Update firestore_service.dart

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -3,8 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class FirestoreService {
   final _firestore = FirebaseFirestore.instance;
 
-  Stream<QuerySnapshot> getMessagesStream() {
-    return _firestore.collection('messages').orderBy('date').limit(10).snapshots();
+  Stream<QuerySnapshot> getMessagesStream({int limit = 10}) {
+    return Stream.empty();
   }
 
   Future<void> addMessage(Map<String, dynamic> message) async {
